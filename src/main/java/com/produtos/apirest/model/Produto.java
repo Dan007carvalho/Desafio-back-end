67467b7a8fad72ee04cc5f992c36fa2a050f4ed6
Daniel Carvalho <dan007@hotmail.com.br>
2021-07-27 22:18:46 -0300

    projeto criado

---
+++ src/main/java/com/produtos/apirest/model/Produto.java
@@ -0,0 +1,109 @@
+package com.produtos.apirest.model;
+
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_PRODUTO")
+public class Produto implements Serializable{
+	private static final long serialVersionUID = 1l;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private long id;
+	
+	private String nome;
+	
+	private double preco;
+	
+	private String codigo;
+	
+	private String categoria;
+	
+	private String status;
+	
+	private String descricao;
+	
+	private Integer quantidade;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+
+	
+	
+	
+	
+}
+	
+	
+	
+	
